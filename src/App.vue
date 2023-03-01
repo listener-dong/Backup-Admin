72228225db2655604af124e47b4607c98631ac21
CXD_self <839823170@qq.com>
2023-03-01 14:50:55 +0800

    chore: 添加tailwind样式拓展工具

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,17 @@ import HelloWorld from "./components/HelloWorld.vue";
 
 <template>
   <!-- 首页 -->
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="bg-bg_color w-full h-full">
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+    <HelloWorld msg="Vite + Vue3" />
   </div>
-  <HelloWorld msg="Vite + Vue3" />
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
-import "./style.css";
+import "./style/index.css";
+// 一定要在main.ts中导入tailwind.css，防止vite每次hmr都会请求src/style/index.scss整体css文件导致热更新慢的问题
+import "./style/tailwind.css";
 import App from "./App.vue";
 
 createApp(App).mount("#app");
